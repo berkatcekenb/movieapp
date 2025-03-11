a804a19afc3bc0ee32cfc5b2df5d6028860e19c3
Berk Atceken <berkatcekenb@gmail.com>
2025-03-11 16:30:41 +0300

    Add admin configurations and model relations

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from movies.models import Genre, Movie, Person, Contact, Video
 
-# Register your models here.
+class MovieAdmin(admin.ModelAdmin):
+    list_display=('title','is_active','is_home')
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('genres','language', 'is_active', 'is_home')
+    search_fields = ('title', 'description')
+
+class PersonAdmin(admin.ModelAdmin):
+    list_display=('full_name', 'gender', 'duty_type')
+    list_filter = ('gender', 'duty_type')
+    search_fields = ('first_name', 'last_name')
+
+admin.site.register(Movie, MovieAdmin)
+admin.site.register(Genre)
+admin.site.register(Person, PersonAdmin)
+admin.site.register(Contact)
+admin.site.register(Video)
--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,91 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
-# Create your models here.
+
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Contact(models.Model):
+    address = models.CharField(max_length=200)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.email
+
+
+class Person(models.Model):
+
+    genres = (
+        ('M', 'Erkek'),
+        ('F', 'Kadın'),
+    )
+
+    duty_types = (
+        ('1', 'Görevli'),
+        ('2', 'Oyuncu'),
+        ('3', 'Yönetmen'),
+        ('4', 'Senarist'),
+    )
+
+
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    biography = models.CharField(max_length=3000)
+    birth_date = models.DateField()
+    gender = models.CharField("Cinsiyet",max_length=1, choices=genres)
+    image_name = models.CharField(max_length=50)
+    duty_type = models.CharField("Görev",max_length=1, choices=duty_types)
+    contacts = models.OneToOneField(Contact, on_delete=models.CASCADE, null=True, blank=True)
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+    
+    full_name.fget.short_description = "Ad Soyad"
+
+    class Meta:
+        verbose_name = "Kişi"
+        verbose_name_plural = "Kişiler"
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} ({self.duty_types[int(self.duty_type)-1][1]})"
+
+class Movie(models.Model):
+    title = models.CharField("Başlık",max_length=100)
+    description = models.TextField("Özet",validators=[MinLengthValidator(20)])
+    image_name = models.CharField("Resim",max_length=50)
+    image_cover = models.CharField(max_length=50)
+    date = models.DateField()
+    slug = models.SlugField(unique=True, db_index=True)
+    budget = models.DecimalField(max_digits=19, decimal_places=2)
+    language = models.CharField(max_length=50)
+    genres = models.ManyToManyField(Genre)
+    is_active = models.BooleanField(default=False)
+    is_home = models.BooleanField(default=False)
+    people = models.ManyToManyField(Person)
+
+    class Meta:
+        verbose_name = "Film"
+        verbose_name_plural = "Filmler"
+
+    def __str__(self):
+        return self.title
+
+
+
+class Video(models.Model):
+    title = models.CharField(max_length=200)
+    video_url = models.CharField(max_length=100)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='videos')
+
+    def __str__(self):
+        return self.title
+
+
+
+
