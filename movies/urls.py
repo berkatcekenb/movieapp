d79b0eb29b43f64b5d9b7a758da377a0f09bacfb
Berk Atceken <berkatcekenb@gmail.com>
2025-03-20 16:06:59 +0300

    fixed some bugs

--- movies/urls.py
+++ movies/urls.py
@@ -4,6 +4,9 @@ from . import views
 urlpatterns = [
     path("", views.index, name="home_page"),
     path("movies", views.movies, name="movies_page"),
-    path("movies/<slug:slug>", views.movie_details, name="movie_details")    
+    path("movies/<slug:slug>", views.movie_details, name="movie_details"),
+    path('people/', views.people, name='people_page'),
+    path('latest-movie/', views.latest_movie, name='latest_movie'),  # Yeni eklenen URL
+    path('search/', views.search_movies, name='search_movies'),
 ]
 
--- movies/views.py
+++ movies/views.py
@@ -1,8 +1,9 @@
 from datetime import date
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
-from movies.models import Movie
+from movies.models import Movie, Person  # Person modelini ekledik
 from .forms import CommentForm
+from django.db.models import Q
 
 data = {
     "sliders": [
@@ -61,4 +62,59 @@ def movie_details(request, slug):
         "videos": movie.videos.all(),
         "comments": comments,
         "comment_form": comment_form
+    })
+
+def latest_movie(request):
+    latest_movie = Movie.objects.filter(is_active=True).order_by('-date').first()
+    if latest_movie:
+        return redirect('movie_details', slug=latest_movie.slug)
+    return redirect('movies_page')
+
+def people(request):
+    actors = Person.objects.filter(duty_type='2')  # Oyuncular
+    directors = Person.objects.filter(duty_type='3')  # Yönetmenler
+    return render(request, 'people.html', {
+        'actors': actors,
+        'directors': directors
+    })
+
+def search_movies(request):
+    query = request.GET.get('q', '')
+    search_type = request.GET.get('type', 'all')  # Varsayılan olarak 'all'
+
+    if query:
+        if search_type == 'movies':
+            results = Movie.objects.filter(
+                Q(title__icontains=query) |
+                Q(description__icontains=query)
+            ).distinct()
+        elif search_type == 'actors':
+            results = Person.objects.filter(
+                Q(duty_type='2') &  # Sadece oyuncular
+                (Q(first_name__icontains=query) | Q(last_name__icontains=query))
+            ).distinct()
+        elif search_type == 'directors':
+            results = Person.objects.filter(
+                Q(duty_type='3') &  # Sadece yönetmenler
+                (Q(first_name__icontains=query) | Q(last_name__icontains=query))
+            ).distinct()
+        else:  # 'all' durumu
+            movies = Movie.objects.filter(
+                Q(title__icontains=query) |
+                Q(description__icontains=query)
+            )
+            people = Person.objects.filter(
+                Q(first_name__icontains=query) | Q(last_name__icontains=query)
+            )
+            results = {
+                'movies': movies,
+                'people': people
+            }
+    else:
+        results = []
+
+    return render(request, 'movies/search.html', {
+        'query': query,
+        'results': results,
+        'search_type': search_type
     })
